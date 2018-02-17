284df1552b6caf40a2d74d5f50554d4d021ed17d
lvalido <lvalido@mailexternos.com.ar>
2018-02-17 17:06:06 -0300

    [IL] Startup commit

---
+++ compile.bat
@@ -0,0 +1,4 @@
+"C:\WINNT\Microsoft.NET\Framework\v4.0.30319\ilasm" /dll "C:\Documents and Settings\A40132\Desktop\il\Model.il"
+"C:\WINNT\Microsoft.NET\Framework\v4.0.30319\ilasm"  "C:\Documents and Settings\A40132\Desktop\il\test.il"
+test.exe
+pause
